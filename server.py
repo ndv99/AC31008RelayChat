cb14fae7c7c07a550079a0141ffb8b5987df05dd
Nick De Villiers <noodles524@gmail.com>
2020-12-08 17:39:51 +0000

    Laid stuff out in server for implementation (I know this is the wrong branch)

--- server.py
+++ server.py
@@ -2,4 +2,38 @@ import os
 import socket
 import select
 
+class Server:
+
+    def __init__(self):
+        pass
+
+    def start_server(self):
+        pass
+
+    def receive(self):
+        pass
+
+    def send_to_server(self):
+        pass
+
+    def send_to_user(self):
+        pass
+
+    def create_channel(self):
+        pass
+
+class ServerChannel:
+
+    def __init__(self):
+        pass
+
+
+class ServerClient:
+
+     def __init__(self):
+         pass
+    
+server = Server()
+server.start_server()
+
 print("This is the server file.")
