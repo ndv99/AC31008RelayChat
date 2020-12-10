19d3b2d39525e2a927f50b6503099c698634e8a2
Nick De Villiers <noodles524@gmail.com>
2020-12-10 22:57:06 +0000

    Commenting to help me understand my own code

--- server.py
+++ server.py
@@ -19,44 +19,42 @@ class Server:
 
         self.socket_list = [self.socket]
         self.clients = {}
-        self.channels = []
-
 
     def start_server(self):
         """Starts the server."""
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.bind((self.ipv4_address, self.port))
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Socket options
+        self.socket.bind((self.ipv4_address, self.port)) # Bind a socket to a given IP and port
 
         print(f"'{self.server_name}' is now running on {self.ipv4_address}:{self.port}.")
 
     def event_loop(self):
-        """Waits to receive messages from clients."""
-        self.socket.listen()
+        """Main even loop for the program"""
+        self.socket.listen() # Listen for messages from clients
         print("Waiting for client...")
 
         while True:
-            read_sockets, _, exception_sockets = select.select(self.socket_list, [], self.socket_list)
+            read_sockets, _, exception_sockets = select.select(self.socket_list, [], self.socket_list) # Checks if any new data can be read from sockets
 
-            for notif_socket in read_sockets:
+            for notif_socket in read_sockets: # If a sokcet has new data to read
                 if notif_socket == self.socket:
-                    client_sckt, client_addr = self.socket.accept()
-                    usr = self.receive_message(client_sckt)
+                    client_sckt, client_addr = self.socket.accept() # Accept a connection from a new client
+                    usr = self.receive_message(client_sckt) # Get user data from that client
 
                     if usr is False:
                         continue
 
-                    self.socket_list.append(client_sckt)
-                    self.clients[client_sckt] = usr
+                    self.socket_list.append(client_sckt) # Add client's socket to socket list
+                    self.clients[client_sckt] = usr # Add client to client dictionary
                     username = usr['data'].decode('utf-8')
                     print(f"{username} has connected to the server.")
 
                 else:
-                    msg = self.receive_message(notif_socket)
-                    if msg is False:
+                    msg = self.receive_message(notif_socket) # Get message from socket
+                    if msg is False: # Msg is false if client terminates connection
                         print(f"Terminated connection with {self.clients[notif_socket]['data'].decode('utf-8')}")
-                        self.socket_list.remove(notif_socket)
-                        del self.clients[notif_socket]
-                        continue
+                        self.socket_list.remove(notif_socket) # Remove client's socket from socket list
+                        del self.clients[notif_socket] # Delete client from client dictionary
+                        continue # Skips the following code to send a message to all clients
                     
                     user = self.clients[notif_socket]
                     username = user['data'].decode("utf-8")
