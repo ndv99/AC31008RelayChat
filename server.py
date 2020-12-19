3af6a29eb4da9baa4bce8b75c53b5d6cad82a7c9
Nick De Villiers <noodles524@gmail.com>
2020-12-19 20:52:07 +0000

    Trying

--- server.py
+++ server.py
@@ -12,11 +12,13 @@ class Server:
 
         self.command_prefix = "$"
         self.header_length = 512
-        self.commands = [
-            "$help: Shows all commands",
-            "$join <channel>: Moves you to <channel>",
-            "$private <user>: Begins private messaging with <user>",
-            "$channels: Displays a list of channels in the server"]
+        # self.commands = [
+        #     "$help: Shows all commands",
+        #     "$join <channel>: Moves you to <channel>",
+        #     "$private <user>: Begins private messaging with <user>",
+        #     "$channels: Displays a list of channels in the server"]
+
+        self.commands = ["JOIN", "PRIVMSG", "NAMES"]
 
         self.server_name = "UoD_IRCServer"
         self.server_name_error = f"{self.server_name}_error"
@@ -32,8 +34,8 @@ class Server:
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        self.socket_list = [self.socket]
-        self.clients = {}
+        self.socket_list = [self.socket] # A list of connected sockets.
+        self.clients = {} # Stores client nicknames with their socket as a key.
         self.public_channels = {
             "#general" : [],
             "#other" : []
@@ -46,6 +48,7 @@ class Server:
         """Starts the server."""
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Socket options
         self.socket.bind((self.ipv4_address, self.port)) # Bind a socket to a given IP and port
+        self.socket.settimeout(5)
 
         print(f"'{self.server_name}' is now running on {self.ipv4_address}:{self.port}.")
 
@@ -53,13 +56,13 @@ class Server:
         message = message.split(" ")
         if message[0] == "JOIN":
             if len(message) > 1:
+                print(message[1])
                 if message[1].lower() not in self.public_channels:
-                    msg_content = f"'{message[1]}' is not a valid channel. Use $channels to get a list of channel names."
-                    msg = self.compose_message(msg_content)
-                    self.send_to_user(self.error_message_metadata, msg, notif_socket)
+                    msg_content = f"ERR_NOSUCHCHANNEL"
+                    self.send_code(msg_content, notif_socket)
                 else:
                     self.remove_client_from_channel(notif_socket)
-                    self.assign_client_channel(message[1].lower, notif_socket)
+                    self.assign_client_channel(message[1].lower(), notif_socket)
         elif message[0] == "NAMES":
             channels = []
             for channel in self.public_channels:
@@ -67,11 +70,11 @@ class Server:
             msg_content = f"Channels: {channels}"
             msg = self.compose_message(msg_content)
             self.send_to_user(self.server_metadata, msg, notif_socket)
-        elif message[0] == "$help":
-            for command in self.commands:
-                msg_content = f"{command}"
-                msg = self.compose_message(msg_content)
-                self.send_to_user(self.server_metadata, msg, notif_socket)
+        # elif message[0] == "$help":
+        #     for command in self.commands:
+        #         msg_content = f"{command}"
+        #         msg = self.compose_message(msg_content)
+        #         self.send_to_user(self.server_metadata, msg, notif_socket)
         elif message[0] == "PRIVMSG":
             if len(message) > 1:
                 uname_list = []
@@ -106,53 +109,46 @@ class Server:
             self.send_to_user(self.error_message_metadata, msg, notif_socket)
 
     def event_loop(self):
-        """Main even loop for the program"""
+        """Main event loop for the program"""
         self.socket.listen() # Listen for messages from clients
         print("Waiting for client...")
 
         while True:
+            print("waiting....")
             read_sockets, _, exception_sockets = select.select(self.socket_list, [], self.socket_list) # Waits for new readable data from sockets
-
+            print("eyy bois we got somthin")
             for notif_socket in read_sockets: # Loop through sockets with new data to read
                 if notif_socket == self.socket: # If the socket is the socket that this server runs on (new connection)
                     client_sckt, client_addr = self.socket.accept() # Accept a connection from a new client and assigns them a unique socket
                     
                     # https://ircv3.net/specs/core/capability-negotiation.htmls
-                    handshake = []
-                    for i in range(2):
-                        msg = self.receive_message(client_sckt) # Get user data from that client
-                        msg = msg.decode('utf-8')[:-2].split('\r\n')
-                        for part in msg:
-                            handshake.append(part)
-                    
-                    print("Loop finished")
-                    print(handshake)
 
-                    nickname = handshake[1].split(" ")[1]
-                    mymessage = f"001 {nickname}: Welcome to the Internet Relay Network {nickname}"
-                    client_sckt.send(mymessage.encode('utf-8'))
-
-                    print("message sent")
-
-                    # if usr is False: # If client disconnected before sending name
-                    #     continue
-
-                    # self.hold_new_client(client_sckt, usr)
+                    nickname = self.handshake(client_sckt)
+                    if nickname:
+                        print("received a new nickname")
+                        success = self.hold_new_client(client_sckt, nickname)
+                        if success:
+                            print("we did it bois")
+                        else:
+                            print("nope")
+                    else:
+                        continue
 
                 else:
                     msg = self.receive_message(notif_socket) # Get message from socket
                     if msg is False: # Msg is false if client terminates connection
-                        print(f"Terminated connection with {self.clients[notif_socket]['data'].decode('utf-8')}")
+                        print(f"Terminated connection with {self.clients[notif_socket]}")
                         self.remove_client_from_channel(notif_socket)
                         self.socket_list.remove(notif_socket) # Remove client's socket from socket list
                         del self.clients[notif_socket] # Delete client from client dictionary
                         continue # Skips the following code to send a message to all clients
                     
                     user = self.clients[notif_socket] # Get sender of the message
-                    username = user['data'].decode("utf-8") # decode username
-                    message = msg['data'].decode("utf-8") # decode message
-                    print(f"Received message from {username}: {message}") # Output in server console
-                    if message[0] == self.command_prefix:
+                    message = msg.decode("utf-8")[:-2] # decode message
+                    print(f"Received message from {user}: {message}") # Output in server console
+
+                    if message.split(" ")[0] in self.commands:
+                        print("Message is command.")
                         self.handle_command(message, notif_socket)
                     else:
                         self.send_to_channel(user, msg, notif_socket) # Send user's message to all other clients in the channel
@@ -162,30 +158,43 @@ class Server:
             self.socket_list.remove(notif_socket)
             del self.clients[notif_socket]
     
+    def handshake(self, client_sckt):
+        print("We made it here")
+        handshake = []
+        for i in range(2):
+            msg = self.receive_message(client_sckt) # Get user data from that client
+            print(msg)
+            if msg:
+                msg = msg.decode('utf-8')[:-2].split('\r\n')
+                for part in msg:
+                    handshake.append(part)
+            else:
+                return False
+
+        nickname = handshake[1].split(" ")[1]
+        return nickname
+    
     def hold_new_client(self, client_sckt, usr): # Checks against username rules in IRC spec
-        username = usr['data'].decode('utf-8')
-        if len(username) > 9 or username[0] == "-":
+        if len(usr) > 9 or usr[0] == "-":
             print("Client tried to connect with invalid username")
-            msg_content = "ERR_ERRONEUSNICKNAME"
-            msg = self.compose_message(msg_content)
-            self.send_to_user(self.server_metadata, msg, client_sckt)
+            code = "432"
+            msg = f"Nickname '{usr}' is invalid."
+            self.send_code(code, client_sckt, msg, usr)
             return False
         
         try:
-            if int(username[0]):
+            if int(usr[0]):
                 print("Client tried to connect with invalid username")
-                msg_content = "ERR_NONICKNAMEGIVEN             "
-                msg = self.compose_message(msg_content)
+                msg = "ERR_NONICKNAMEGIVEN"
                 self.send_to_user(self.server_metadata, msg, client_sckt)
                 return False
         except ValueError:
             pass
         
         for client in self.clients:
-            if self.clients[client]['data'].decode('utf-8') == username:
+            if self.clients[client].decode('utf-8') == usr:
                 print("Client tried to connect with invalid username")
-                msg_content = "ERR_NICKCOLLISION"
-                msg = self.compose_message(msg_content)
+                msg = "ERR_NICKCOLLISION"
                 self.send_to_user(self.server_metadata, msg, client_sckt)
                 return False
 
@@ -193,11 +202,11 @@ class Server:
         self.socket_list.append(client_sckt) # Add client's socket to socket list
         self.clients[client_sckt] = usr # Add client to client dictionary
 
-        msg_content = "conn_accepted"
-        msg = self.compose_message(msg_content)
-        self.send_to_user(self.server_metadata, msg, client_sckt)
-        print(f"New client {usr['data'].decode('utf-8')} has connected.")
-        self.assign_client_channel('general', client_sckt)
+        msg = f"Welcome to the Internet Relay Network, {usr}."
+        self.send_to_server(self.server_name, msg, client_sckt)
+        print(f"New client {usr} has connected.")
+        # self.assign_client_channel('#general', client_sckt)
+        return True
     
     def compose_message(self, content):
         msg_content = content.encode('utf-8')
@@ -217,49 +226,60 @@ class Server:
             users_in_channel.append(uname)
 
         self.public_channels[channel_name].append(client_sckt)
-        msg_content = f"You are in {channel_name} with: {users_in_channel}"
-        msg = self.compose_message(msg_content)
-        self.send_to_user(self.server_metadata, msg, client_sckt)
+        msg_content = "JOIN"
+        self.send_code(msg_content, client_sckt)
+        msg_content = f"RPL_TOPIC none"
+        self.send_code(msg_content, client_sckt)
+        msg_content = f"RPL_NAMREPLY {self.get_channel_usernames(channel_name)}"
+        self.send_code(msg_content, client_sckt)
         
-        msg_content = f"{self.get_username(client_sckt)} has joined {channel_name}"
-        msg = self.compose_message(msg_content)
-        self.send_to_channel(self.server_metadata, msg, client_sckt)
+        # msg_content = f"{self.get_username(client_sckt)} has joined {channel_name}"
+        # self.send_to_channel(self.server_name, msg, client_sckt)
 
-    
     def remove_client_from_channel(self, notif_socket):
         priv_channel = True
-        for channel in self.public_channels:
-            if notif_socket in self.public_channels[channel]:
-                priv_channel = False
-                msg_content = f"{self.get_username(notif_socket)} has left {channel}"
-                msg = self.compose_message(msg_content)
-                self.send_to_channel(self.server_metadata, msg, notif_socket)
-                self.public_channels[channel].remove(notif_socket)
+        try:
+            for channel in self.public_channels:
+                if notif_socket in self.public_channels[channel]:
+                    priv_channel = False
+                    msg_content = f"{self.get_username(notif_socket)} has left {channel}"
+                    msg = self.compose_message(msg_content)
+                    self.send_to_channel(self.server_metadata, msg, notif_socket)
+                    self.public_channels[channel].remove(notif_socket)
+        except KeyError:
+            pass
         
-        if priv_channel:
-            clients = []
-            private_channel = None
-            for channel in self.private_channels:
-                if notif_socket in self.private_channels[channel]:
-                    private_channel = channel
-                    for client_socket in self.private_channels[channel]:
-                        clients.append(client_socket)
-            
-            for client_socket in clients:
-                if client_socket != notif_socket:
-                    self.assign_client_channel('general', client_socket)
-                self.private_channels[private_channel].remove(client_socket)
-            del self.private_channels[private_channel]
+        try:
+            if priv_channel:
+                clients = []
+                private_channel = None
+                for channel in self.private_channels:
+                    if notif_socket in self.private_channels[channel]:
+                        private_channel = channel
+                        for client_socket in self.private_channels[channel]:
+                            clients.append(client_socket)
+                
+                for client_socket in clients:
+                    if client_socket != notif_socket:
+                        self.assign_client_channel('general', client_socket)
+                    self.private_channels[private_channel].remove(client_socket)
+                del self.private_channels[private_channel]
+        except KeyError:
+            pass
 
-    
     def get_username(self, client_sckt):
-        user = self.clients[client_sckt]['data'].decode('utf-8')
-        return user
+        return self.clients[client_sckt]
+
+    def get_channel_usernames(self, channel_name):
+        names = []
+        for sckt in self.public_channels[channel_name]:
+            names.append(self.clients[sckt])
+        
+        return names
 
     def receive_message(self, client_sckt):
         try:
             msg = client_sckt.recv(4096) # Receive message
-            print(msg.decode('utf-8'))
             if not len(msg): # Return false if there's no data received
                 return False
 
@@ -269,12 +289,9 @@ class Server:
             print(e)
             return False
 
-
     def send_to_server(self, author, msg, notif_socket):
         for client_sckt in self.clients:
-            if client_sckt != notif_socket:
-                # Send username and message, as well as headers
-                client_sckt.send(author['header'] + author['data'] + msg['header'] + msg['data'])
+            client_sckt.send(f"{author}: {msg}\r\n".encode('utf-8'))
     
     def send_to_channel(self, author, msg, notif_socket):
         # find client's channel
@@ -296,8 +313,10 @@ class Server:
                             client_sckt.send(author['header'] + author['data'] + msg['header'] + msg['data'])
 
     def send_to_user(self, author, msg, client_sckt):
-        client_sckt.send(author['header'] + author['data'] + msg['header'] + msg['data'])
-
+        client_sckt.send(f"{author}: {msg}\r\n".encode('utf-8'))
+    
+    def send_code(self, code, client_sckt, msg, usr):
+        client_sckt.send(f":{self.ipv4_address} {code} * {usr} :{msg}\r\n".encode('utf-8'))
     
 if __name__ == "__main__":
     server = Server()
