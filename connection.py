31276eb46459751b780cae5f6a87be8d0ed39c50
Nick De Villiers <noodles524@gmail.com>
2020-12-20 17:26:40 +0000

    Formatting and minor bug fixes.

--- connection.py
+++ connection.py
@@ -1,6 +1,6 @@
 class Connection:
     """A connection to the IRC server in server.py.
-    
+
     Args:
         sckt - A socket object
         addr - The address that the connection is coming from
@@ -15,7 +15,7 @@ class Connection:
             addr (str): The IP address of the new connection (IPv6) and the port (6667 for IRC).
             mem (server_mem.Memory): An object containing the server memory.
         """
-        
+
         self.socket = sckt
         self.address = addr[0]
         self.nickname = ""
@@ -32,7 +32,9 @@ class Connection:
             "QUIT",
             "JOIN",
             "PART",
-            "PRIVMSG"
+            "PRIVMSG",
+            "WHO",
+            "MODE"
         ]
 
         self.cached_command = None
@@ -40,7 +42,7 @@ class Connection:
     def loop(self):
         """The main event loop for the connection.
         """
-        
+
         try:
             while True:
                 msg = self.socket.recv(4096)
@@ -51,13 +53,15 @@ class Connection:
                     for part in msg:
                         if part:
                             self.check_command(part)
+
+                    # So this little bit of code is basically to cache the USER command until the nickname has been verified for the first time.
                     if self.cached_command is not None:
                         self.check_command(None, self.cached_command)
         except ConnectionResetError:
-            print("connection reset")
+            print("Client disconnected. Connection reset.")
         except BrokenPipeError:
-            print("Broken pipe")
-    
+            print("Broken pipe (how the fuck did this happen?!)")
+
     def check_command(self, cmd_string, cmd_split=None):
         """Checks a command sent from a client
 
@@ -65,7 +69,7 @@ class Connection:
             cmd_string (string): A command given as a string (default)
             cmd_split (list, optional): Effectively cmd_string but split by a space. Defaults to None. Only needed for cached commands.
         """
-          
+
         if cmd_split is not None:
             cmd = cmd_split
         else:
@@ -92,7 +96,7 @@ class Connection:
                 self.leave_channel(cmd[1])
         else:
             print("Unknown command.")
-    
+
     def set_nickname(self, nickname):
         """Attempts to set a new nickname specified by the cient.
 
@@ -102,14 +106,14 @@ class Connection:
         Returns:
             bool: True if successful, False if not.
         """
-            
+
         if len(nickname) > 9 or nickname[0] == "-":
             print("Client tried to connect with invalid username")
             code = "432"
             msg = f":{nickname}"
             self.send_code(code, nickname, msg)
             return False
-        
+
         try:
             if int(nickname[0]):
                 print("Client tried to connect with invalid username")
@@ -119,7 +123,7 @@ class Connection:
                 return False
         except ValueError:
             pass
-        
+
         for client in self.server_mem.clients:
             if self.server_mem.clients[client] == nickname:
                 print("Client tried to connect with taken username")
@@ -127,15 +131,15 @@ class Connection:
                 msg = f":{nickname}"
                 self.send_code(code, nickname, msg)
                 return False
-        
+
         self.nickname = nickname
         self.nick_set = True
-        
+
         msg = f"NICK {nickname}"
         self.send_message_from_server(msg)
         self.server_mem.clients[self.socket] = self.nickname
         return True
-    
+
     def set_realname(self, name):
         """Sets the real name (username) of the client. Welcomes the client since username can only be set upon login.
 
@@ -144,7 +148,7 @@ class Connection:
         """
 
         self.realname = name
-        
+
         self.server_mem.clients[self.socket] = self.nickname
         self.server_mem.socket_list.append(self.socket)
 
@@ -172,7 +176,7 @@ class Connection:
             for socket in self.server_mem.channels[chan]:
                 msg = f"{self.nickname}!{self.realname}@{self.address} JOIN {chan}"
                 self.send_message(socket, msg)
-            
+
             msg = f"332 {chan} :{chan} no topic"
             self.send_message_from_server(msg)
 
@@ -185,7 +189,7 @@ class Connection:
 
             msg = f"366 {chan} :End of NAMES list"
             self.send_message_from_server(msg)
-    
+
     def leave_channel(self, chan):
         """Removes a client from a channel.
 
@@ -194,7 +198,7 @@ class Connection:
         """
 
         if chan in self.server_mem.channels:
-            if sckt in self.server_mem.channels[chan]:
+            if self.socket in self.server_mem.channels[chan]:
                 self.server_mem.channels[chan].remove(self.socket)
                 for socket in self.server_mem.channels[chan]:
                     msg = f"{self.nickname}!{self.realname}@{self.address} PART {chan}"
@@ -212,12 +216,13 @@ class Connection:
         Args:
             chan (string): The name of the channel/user to send to.
             msg (string): The message to be sent.
-        """        
-        if chan[0] == "#": # if target is a channel
+        """
+
+        if chan[0] == "#":  # if target is a channel
             for client in self.server_mem.channels[chan]:
                 if client != self.socket:
                     self.send_message_from_user(client, msg, chan)
-        else: # if target is a user
+        else:  # if target is a user
             found = False
             for client in self.server_mem.clients:
                 if self.server_mem.clients[client] == chan:
@@ -231,19 +236,22 @@ class Connection:
             code (string): The code to be sent (numeric, see IRC documentation https://tools.ietf.org/html/rfc1459)
             usr (string): The name of the user that the code is being sent to.
             msg (string): Any additional text to be sent after the code.
-        """        
-        self.socket.send(f":{self.server_mem.ipv6_address} {code} {usr} {msg}\r\n".encode())
+        """
+
+        self.socket.send(
+            f":{self.server_mem.ipv6_address} {code} {usr} {msg}\r\n".encode())
         print(f":{self.server_mem.ipv6_address} {code} {usr} {msg}\r\n")
-    
+
     def send_message_from_server(self, msg):
         """Sends a message from the server to the client.
 
         Args:
             msg (string): The message to be sent.
-        """        
+        """
+
         self.socket.send(f":{self.server_mem.ipv6_address} {msg}\r\n".encode())
         print(f":{self.server_mem.ipv6_address} {msg}\r\n")
-    
+
     def send_message_from_user(self, sckt, msg, chan):
         """Sends a message from the client to another user.
 
@@ -251,9 +259,12 @@ class Connection:
             sckt (socket.Socket): The recipient's socket.
             msg (string): The message to be sent.
             chan (string): The target channel of the message (either a username or a channel name).
-        """        
-        sckt.send(f":{self.nickname}!{self.realname}@{self.address} PRIVMSG {chan} {msg}\r\n".encode())
-        print(f"from {self.nickname} to {chan}:{self.nickname}!{self.realname}@{self.address} PRIVMSG {chan} {msg}")
+        """
+
+        sckt.send(
+            f":{self.nickname}!{self.realname}@{self.address} PRIVMSG {chan} {msg}\r\n".encode())
+        print(
+            f"from {self.nickname} to {chan}:{self.nickname}!{self.realname}@{self.address} PRIVMSG {chan} {msg}")
 
     def send_message(self, sckt, msg):
         """Sends a message from this socket to another socket. Mainly for debugging.
@@ -261,6 +272,7 @@ class Connection:
         Args:
             sckt (socket.Socket): The recipient's socket.
             msg (string): The message to be sent.
-        """        
+        """
+
         sckt.send(f":{msg}\r\n".encode())
         print(f"sent this:{msg}\r\n")
